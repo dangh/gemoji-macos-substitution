dba460dd5fe9e6742da638dc857d9ffdd2b92f26
Dang H <huynhminhdang@gmail.com>
2020-02-13 02:49:05 +0800

    generate personal substitutions

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const plist = require('plist');
+const gemoji = require('gemoji');
+const emoticon = require('emoticon');
+const personal = require('./personal');
+
+console.log(new Set(gemoji.map(x => x.category)));
+
+let gemojies = gemoji
+  .filter(g => ['Smileys & Emotion'].includes(g.category))
+  .flatMap(({ emoji, names }) =>
+    names.map(name => ({ phrase: emoji, shortcut: ':' + name + ':' }))
+  );
+let emoticons = emoticon.flatMap(({ emoji, emoticons }) => {
+  if (emoji == '😗') emoji = '😙';
+  return emoticons.map(emoticon => ({ phrase: emoji, shortcut: emoticon }));
+});
+let personals = personal.flatMap(({ phrase, shortcuts }) =>
+  shortcuts.map(shortcut => ({ phrase, shortcut }))
+);
+let all = [].concat(personals, emoticons, gemojies);
+
+generatePlist('gemoji', gemojies);
+generatePlist('emoticon', emoticons);
+generatePlist('personal', personals);
+generatePlist('all', all);
+
+function generatePlist(name, substitutions) {
+  fs.writeFileSync('./' + name + '.plist', plist.build(substitutions));
+}
